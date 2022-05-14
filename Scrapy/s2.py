d5266649f0c0d91f5a514092445fcfdaac39294e
Meggie2 <100377953+Meggie2@users.noreply.github.com>
2022-05-14 20:37:59 +0200

    Update s2.py

--- Scrapy/s2.py
+++ Scrapy/s2.py
@@ -1,5 +1,7 @@
 import scrapy
 
+## definition of all element which will be scraped
+
 class hotels(scrapy.Item):
     Name        = scrapy.Field()
     Price = scrapy.Field()
@@ -9,10 +11,13 @@ class hotels(scrapy.Item):
     Service_Quality = scrapy.Field()
     Security = scrapy.Field()
     Location = scrapy.Field()
-
+    
+## spider creation
 class LinkListsSpider(scrapy.Spider):
     name = 'hotels'
     allowed_domains = ['https://hotels.ng/']
+    
+## import of links saved using spider s1
     try:
         with open("links.csv", "rt") as f:
             start_urls = [url.strip() for url in f.readlines()][1:]
@@ -22,6 +27,7 @@ class LinkListsSpider(scrapy.Spider):
     def parse(self, response):
         p = hotels()
 
+## Xpath method used to extract needed information 
         name_xpath        = '//h1/text()'
         price_xpath = '//*[@id="bookOnline"]/div[1]/div[4]/div[2]/div[1]/div[1]/div[2]/div/p[1]//text()'
         rating_xpath = '//*[@id="reviews"]/div[2]/div[1]/div/div[1]//text()'
@@ -31,6 +37,7 @@ class LinkListsSpider(scrapy.Spider):
         security_xpath       = '//*[@id="reviews"]/div[2]/div[2]/div[4]/p[2]//text()'
         location_xpath       = '//*[@id="reviews"]/div[2]/div[2]/div[5]/p[2]//text()'
 
+## Extracting all date for each column and row
 
         p['Name']               = response.xpath(name_xpath).getall()
         p['Price']= response.xpath(price_xpath).getall()
